9fdc4d18f6e5b9ce5653c0b133a951c7a2a9f230
caffene-query <caffenequery@gmail.com>
2021-08-18 09:47:42 -0400

    bug fix 1

--- NetManagerConvo.cs
+++ NetManagerConvo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text;
 using System.Net;
